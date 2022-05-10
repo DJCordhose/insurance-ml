620bd87b7b750209f85e0ad3550d948c6e1629bb
Oliver Zeigermann <oliver.zeigermann@openknowledge.de>
2022-05-10 16:10:53 +0200

    experiment to fix broken model

--- app/tf_prediction.py
+++ app/tf_prediction.py
@@ -3,8 +3,9 @@ import tensorflow as tf
 # loading models only once on startup
 # https://www.tensorflow.org/guide/keras/save_and_serialize
 
-# model = tf.keras.models.load_model("classifier")
-model = tf.keras.models.load_model("C:/Users/olive/Development/insurance-ml/app/classifier")
+model = tf.keras.models.load_model("/python_server/classifier")
+# model = tf.keras.models.load_model("C:/Users/olive/Development/insurance-ml/app/classifier")
+# model = tf.keras.models.load_model("C:/Users/oliver.zeigermann/Development/insurance-ml/app/classifier")
 
 def predict(speed, age, miles):
     sample = [[age, speed]]
