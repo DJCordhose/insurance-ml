9174cc624b6f9ef5490a0b5d9563b60b8da49e34
Oliver Zeigermann <oliver.zeigermann@openknowledge.de>
2022-05-15 17:21:13 +0200

    example for documentation

--- lib/InsuranceModel.py
+++ lib/InsuranceModel.py
@@ -1,6 +1,11 @@
 import tensorflow as tf
 
 class InsuranceModel:
+    """Class for model execution.
+    
+    Args:
+        model: can be passed or loaded later
+    """
     def __init__(self, model=None, age_range=(10, 150), max_speed_range=(50, 250), keras_format=True):
         self.model = model
         self.keras_format = keras_format
@@ -27,7 +32,7 @@ class InsuranceModel:
         else:
             self.model.save(model_path, save_format='tf')
 
-    def check_range(self, age, speed, throw=False):
+    def check_range(self, age: int, speed: int, throw=False):
         min_age, max_age = self.age_range 
         min_speed, max_speed = self.max_speed_range 
         valid = age >= min_age and age <= max_age and speed >= min_speed and speed <= max_speed
@@ -35,11 +40,23 @@ class InsuranceModel:
             raise ValueError(f'age {age} and/or max_speed {max_speed} outside of valid range')
         return valid
 
-    def predict(self, age, max_speed):
+    def predict(self, age: int, max_speed: int):
+        """Predicts probabilities and category
+
+        Returns:
+            List[float]: list of probabilities per category
+            int: predicted category
+        """
         probas = self.predict_proba(age, max_speed)
         return probas[0].tolist(), int(probas.argmax())
 
-    def predict_proba(self, age, max_speed):
+    def predict_proba(self, age: int, max_speed:int):
+        """Predicts normalized probabilities per category
+
+        Returns:
+            List[float]: list of probabilities per category
+        """
+
         self.check_range(age, max_speed, throw=True)
         X = [[age, max_speed]]
         return self.model.predict(X)
