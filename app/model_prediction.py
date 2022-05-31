8d6cbf0e76d66467411c224bbf066f549ccfdd5e
Oliver Zeigermann <oliver.zeigermann@openknowledge.de>
2022-05-31 15:16:50 +0200

    wip

--- app/model_prediction.py
+++ app/model_prediction.py
@@ -3,8 +3,8 @@ from InsuranceModel import InsuranceModel
 insurance_model = InsuranceModel()
 
 # model_path = "/home/olli/insurance-ml/app/classifier"
-# model_path = "/home/olli/insurance-ml/app/keras_model"
-model_path = "INSERT PATH TO YOUR MODEL HERE"
+model_path = "/home/olli/insurance-ml/app/keras_model"
+# model_path = "INSERT PATH TO YOUR MODEL HERE"
 # model_path = "/python_server/classifier"
 insurance_model.load_model(model_path, keras_format=True)
 
